531c0d926ba1258587a9bb9e959da19bb0379baa
Travis Whitaker <pi.boy.travis@gmail.com>
2013-08-09 15:23:09 +0000

    Fixed header reference.

--- src/rdfgen.c
+++ src/rdfgen.c
@@ -43,7 +43,7 @@ int getColNames(char *firstline, char *columnames)
 
 void outputHeader(FILE *outputfile, char *tablename, int colnum, char *columnames)
 {
-	fprintf(outputfile,"<?xml version=\"1.0\"?>\n<!DOCTYPE rdf:RDF [<!ENTITY xsd \"http://www.w3.org/2001/XMLSchema#\">]>\n<rdf:RDF\n  xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n  xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n  xml:base=\"http://example.org/schemas/vehicles\"\n  xmlns:dwh=\"http://www.jnj.com/dwh#\">\n<rdfs:Datatype rdf:about=\"&xsd;string\">\n\n<rdfs:Class rdf:ID=\"");
+	fprintf(outputfile,"<?xml version=\"1.0\"?>\n<!DOCTYPE rdf:RDF [<!ENTITY xsd \"http://www.w3.org/2001/XMLSchema#\">]>\n<rdf:RDF\n  xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n  xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n  xml:base=\"http://www.jnj.com/dwh/tables#\"\n  xmlns:dwh=\"http://www.jnj.com/dwh#\">\n<rdfs:Datatype rdf:about=\"&xsd;string\">\n\n<rdfs:Class rdf:ID=\"");
 
 	fprintf(outputfile,"%s",tablename);
 	fprintf(outputfile,"\"/>\n\n");
