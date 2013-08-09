6e46ce47b1baf97c7d69f4e85314601e5ce16a36
Travis Whitaker <pi.boy.travis@gmail.com>
2013-08-09 12:58:08 +0000

    Better error messages.

--- src/main.c
+++ src/main.c
@@ -18,31 +18,31 @@ int main(int argc,char *argv[])
 	char *inputfilename = malloc(100);
 	if(inputfilename == NULL)
 	{
-		printf("Couldn't allocate memory for *inputfilename.\n");
+		printf("Couldn't allocate memory for *inputfilename.\nFATAL ERROR\n");
 		return 0;
 	}
 	char *outputfilename = malloc(35);
 	if(outputfilename == NULL)
 	{
-		printf("Couldn't allocate memory for *outputfilename.\n");
+		printf("Couldn't allocate memory for *outputfilename.\nFATAL ERROR\n");
 		return 0;
 	}
 	char *tablename = malloc(31);
 	if(tablename == NULL)
 	{
-		printf("Couldn't allocate memory for *tablename.\n");
+		printf("Couldn't allocate memory for *tablename.\nFATAL ERROR\n");
 		return 0;
 	}
 
 	//Make sure there is only one argument:
 	if(argv[1] == NULL)
 	{
-		printf("Too few arguments.\n");
+		printf("Too few arguments.\nUsage: rdfgen filename");
 		return 0;
 	}
 	else if(argv[2] != NULL)
 	{
-		printf("Too many arguments.\n");
+		printf("Too many arguments.\nUsage: rdfgen filename");
 		return 0;
 	}
 
@@ -55,29 +55,31 @@ int main(int argc,char *argv[])
 	inputfile = fopen(inputfilename,"r");
 	if(inputfile == NULL)
 	{
-		printf("Couldn't open %s\n",inputfilename);
+		printf("Couldn't open %s\nFATAL ERROR\n",inputfilename);
 		return 0;
 	}
 	free(inputfilename);
 	outputfile = fopen(outputfilename,"w");
 	if(outputfile == NULL)
 	{
-		printf("Couldn't open %s\n",outputfilename);
+		printf("Couldn't open %s\nFATAL ERROR\n",outputfilename);
 		return 0;
 	}
 	free(outputfilename);
 
 	//Allocate memory for the 2D array of column names:
-	char *columnames = malloc(31000); //30 chars * 1000 max cols
+	char *columnames = malloc(30000); //30 chars * 1000 max cols
 	if(columnames == NULL)
 	{
-		printf("Couldn't allocate memory for *columnames.\n");
+		printf("Couldn't allocate memory for *columnames.\nFATAL ERROR\n");
+		return 0;
 	}
 
 	char *firstline = malloc(512000);
 	if(columnames == NULL)
 	{
-		printf("Couldn't allocate memory for *firstline.\n");
+		printf("Couldn't allocate memory for *firstline.\nFATAL ERROR\N");
+		return 0;
 	}
 	getLine(inputfile,firstline,512000);
 
