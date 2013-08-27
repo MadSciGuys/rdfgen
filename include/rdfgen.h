681c61e72e84940c05df11325ca9f8eee4f80edf
Travis Whitaker <twhitak@its.jnj.com>
2013-08-27 20:51:47 +0000

    Huge commit. Basically re-writing the program to add features needed by MAXXAM and other data. There are now extensive comments. Read the new header file.

--- include/rdfgen.h
+++ include/rdfgen.h
@@ -4,33 +4,74 @@
 #ifndef RDFGEN_H
 #define RDFGEN_H
 
-#define MAXFILENAME 256
-#define MAXTABLENAME 30
+// Here we define program-wide parameters that vary according to the environment
+// and application. We've chosen default parameters for processing CSV files
+// sourced from Oracle tables on a UNIX-like machine.Note that these are
+// compiler macros, NOT variables. They are simply found and replaced in the
+// source code text at compile time. These can be easily identified in the
+// source by their capitalization and use of underscores. All field lengths
+// are in bytes and disregarding the terminating null byte.
 
-#define RDFEXT ".rdf"
-#define RDFEXTLEN 5
+// Define booleans:
 
-#define MAXCOLS 1000
+#define TRUE 0xff
+#define FALSE 0x00
+
+// Define parameters of the output extension:
+
+#define RDF_EXT ".rdf"
+#define RDF_EXT_LEN 4
+
+// Define the max file name length, this will be OS-specific:
+
+#define MAX_FILE_NAME_LEN 256
+
+// Define the max lengths of table names and column names:
+
+#define MAX_TABLE_NAME_LEN 30
+#define MAX_COLUMN_NAME_LEN 30
+
+// Define the max number of columns and max length of data fields
+
+#define MAX_COLUMNS 1000
+#define MAX_FIELD_LEN 4000
+
+// Define the flags used with the open() system call:
+#define O_READ_FLAGS O_RDONLY
+
+
+
+// Here we define the data structures that are specific to rdfgen.
+
+// Define structure to hold column-specific information:
 
 typedef struct
 {
-	char name[MAXTABLENAME+1];
-	char FK[MAXTABLENAME+1];
-} colname_t;
+	char columnName[MAX_COLUMN_NAME_LEN+1]; // Name of the column.
+	char FKtarget[MAX_TABLE_NAME_LEN+1]; // If the column is an FK, this contains the name of the target. Null otherwise.
+} column_t;
 
-void outputFilename(char *inputfilename, char *outputfilename, char *tablename);
-int getColNames(FILE *inputfile, colname_t *columnames);
-int resolveFK(char *anon, FILE *schmafile, char *tablename, colname_t *columnames);
-void outputHeader_leaf(FILE *outputfile, char *tablename, int colnum, colname_t *columnames);
-void outputFooter(FILE *outputfile);
-int outputTriples_leaf(FILE *outputfile, FILE *inputfile, char *tablename, int colnum, colname_t *columnames);
-void outputHeader_leaf_anon(FILE *outputfile, char *tablename, int colnum, colname_t *columnames);
-int outputTriples_leaf_anon(FILE *outputfile, FILE *inputfile, char *tablename, int colnum, colname_t *columnames);
+// Define structure to hold table-specific information:
 
-void outputHeader(FILE *outputfile, char *tablename, int colnum, colname_t *columnames);
-int outputTriples(FILE *outputfile, FILE *inputfile, char *tablename, int colnum, colname_t *columnames);
-void outputHeader_anon(FILE *outputfile, char *tablename, int colnum, colname_t *columnames);
-int outputTriples_anon(FILE *outputfile, FILE *inputfile, char *tablename, int colnum, colname_t *columnames);
+typedef struct
+{
+	char tableName[MAX_TABLE_NAME_LEN+1]; // Name of the table.
+	int totalColumns; // Total number of columns in the table.
+	column_t columns[MAX_COLUMNS]; // List of columns in the table.
+	int primaryIdentifier; // Index in columns[] of the table's primary identifier. If negative, table has no primary identifier.
+	char foreignIdentifier[MAX_TABLE_NAME_LEN+1]; // If the primary identifier is a foreign key, this contains the name of the target. Null otherwise.
+	char isLeaf; // True if the table has no foreign keys.
+} table_t;
+
+// Define structure to hold one field's worth of data for buffering:
+
+typedef struct
+{
+	char data[MAX_FIELD_LEN+1]; // One field or cell's worth of data.
+} field_t;
+
+// Function declarations:
+int getTableName(char *inputfilename, char *outputfilename, table_t *table);
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,13 +1,107 @@
 // Travis Whitaker 2013
 // twhitak@its.jnj.com
 
+// Standard IO and string manipulation:
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
+// Memory management, kernel features and types:
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+// Kernel level IO:
+#include <fcntl.h>
+
+// rdfgen structures and functions:
 #include <rdfgen.h>
 
+
+
 int main(int argc,char *argv[])
+{
+	// First make sure there are at least two arguments, fail otherwise:
+	if(argc < 3)
+	{
+		printf("TOO FEW ARGUMENTS!\nUsage:\nrdfgen schemafile csvfile1 csvfile2 csvfile3 ...\n");
+		return 1;
+	}
+
+	// Declare file descriptors:
+	int schemafile_fd;
+	int inputfile_fd;
+
+	// The output file is not memory mapped, so it gets a file pointer:
+	FILE *outputfile;
+	// Save a little memory for the output filename:
+	char outputfilename[MAX_TABLE_NAME_LEN+RDF_EXT_LEN+1];
+
+	// Pointers for memory maps:
+	char *schemafile_map;
+	char *inputfile_map;
+
+	// fstat() writes it's result to these structs:
+	struct stat schemafile_stat;
+	struct stat inputfile_stat;
+
+
+	// The schema file's map persists across the processing of input files,
+	// so we'll handle it outside of the main loop.
+	// First ask the kernel to create the fd:
+	schemafile_fd = open(argv[1],O_READ_FLAGS);
+	// Make sure that worked:
+	if(schemafile_fd == -1)
+	{
+		printf("File I/O error!\nUnable to open schema file %s\nFATAL ERROR\n",argv[1]);
+		return 1;
+	}
+	// Ask the kernel to stat the file:
+	if(fstat(schemafile_fd,&schemafile_stat) == -1)
+	{
+		printf("File I/O error!\nUnable to stat schema file %s\nFATAL ERROR\n",argv[1]);
+		return 1;
+	}
+	// Ask the kernel to map the memory:
+	schemafile_map = mmap(NULL,schemafile_stat.st_size,PROT_READ,MAP_PRIVATE,schemafile_fd,0);
+	if(schemafile_map == MAP_FAILED)
+	{
+		printf("Memory I/O error!\nUnable to create map for schema file %s\nFATAL ERROR\n",argv[1]);
+		return 1;
+	}
+	// Got this far, get ready for the main loop.
+	// Allocate memory for the table metadata:
+	table_t *table = malloc(sizeof(table_t))
+	if(table_t == NULL)
+	{
+		printf("Memory I/O error!\nUnable to allocate memory for table metadata.\nFATAL ERROR\n");
+		return 1;
+	}
+	// Allocate memory for a row buffer:
+	field_t *row_buffer = malloc(MAX_COLUMNS*sizeof(field_t));
+	if(row_buffer == NULL)
+	{
+		printf("Memory I/O error!\nUnable to allocate memory for row buffer.\nFATAL ERROR\n");
+		return 1;
+	}
+	// Ready for the main loop. We're going to iterate over the remaining
+	// arguments:
+	for(int currentArg = 2;currentArg < argc;currentArg++)
+	{
+		// Get the table name from the input file name:
+		if(getTableName(argv[currentArg],outputfilename,table) == -1)
+		{
+			printf("FATAL ERROR\n");
+			return 1;
+		}
+	
+	
+	return 0;
+}
+
+/*
+int oldmain(int argc,char *argv[])
 {
 	FILE *inputfile;
 	FILE *outputfile;
@@ -139,3 +233,4 @@ int main(int argc,char *argv[])
 	fclose(outputfile);
 	return 0;
 }
+*/
