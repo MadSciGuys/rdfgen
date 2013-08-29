34cc366795099b0b3fd2d2ab357445e06de57e59
Travis Whitaker <twhitak@its.jnj.com>
2013-08-29 17:41:47 +0000

    Finished main.c and rdfgen.h

--- include/rdfgen.h
+++ include/rdfgen.h
@@ -41,7 +41,7 @@
 // Define structure to hold one field's worth of data for buffering:
 typedef struct
 {
-	char data[MAX_FIELD_LEN+1]; // One field or cell's worth of data.
+	char data[ MAX_FIELD_LEN + 1 ]; // One field or cell's worth of data.
 } field_t;
 
 // Define enum for column type:
@@ -56,22 +56,26 @@ typedef enum
 typedef struct
 {
 	column_type_t type; // Column type.
-	char columnName[MAX_COLUMN_NAME_LEN+1]; // Name of the column.
-	char FKtarget[MAX_TABLE_NAME_LEN+1]; // If the column is an FK, this contains the name of the target. Null otherwise.
+	char columnName[ MAX_COLUMN_NAME_LEN + 1 ]; // Name of the column.
+	char FKtarget[ MAX_TABLE_NAME_LEN + 1 ]; // If the column is an FK, this contains the name of the target. Null otherwise.
 	field_t defaultValue; // Default column value if applicable.
 } column_t;
 
 // Define structure to hold table-specific information:
 typedef struct
 {
-	char tableName[MAX_TABLE_NAME_LEN+1]; // Name of the table.
+	char tableName[ MAX_TABLE_NAME_LEN + 1 ]; // Name of the table.
 	int totalColumns; // Total number of columns in the table.
-	column_t columns[MAX_COLUMNS]; // List of columns in the table.
+	column_t columns[ MAX_COLUMNS ]; // List of columns in the table.
 	int primaryIdentifier; // Index in columns[] of the table's primary identifier. If negative, table has no primary identifier.
 } table_t;
 
 // Function declarations:
-int getTableName(char *inputfilename, char *outputfilename, table_t *table);
-
+int getTableName( char* inputfilename, char* outputfilename, table_t* table );
+int checkEmpty( char* inputfile_map );
+int getColumnNames( char* inputfile_map, table_t* table );
+int getTableMetadata( char* schemafile_map, table_t* table);
+void outputHeader( FILE* outputfile, table_t* table );
+void outputTriples( FILE* outputfile, char* inputfile_map, table_t* table, char* row_buffer );
 
 #endif
