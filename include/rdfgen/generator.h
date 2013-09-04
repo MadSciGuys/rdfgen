156726b09aa53e95faa7f27a886a554a763ea1cf
Travis Whitaker <twhitak@its.jnj.com>
2013-09-04 19:20:45 +0000

    Added generator.c/generator.h

---
+++ include/rdfgen/generator.h
@@ -0,0 +1,7 @@
+// Travis Whitaker 2013
+// twhitak@its.jnj.com
+
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#endif
---
+++ src/rdfgen/generator.c
@@ -0,0 +1,14 @@
+// Travis Whitaker 2013
+// twhitak@its.jnj.com
+
+// Standard I/O functions:
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <rdfgen/limits.h>
+#include <rdfgen/parser.h>
+#include <rdfgen/interface.h>
+
+
+
