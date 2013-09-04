17b89132fb4e1ec0b22aca5d7a47f1d4c547e669
Travis Whitaker <twhitak@its.jnj.com>
2013-09-04 14:36:02 +0000

    Fixed typo in header file.

--- include/rdfgen/parser.h
+++ include/rdfgen/parser.h
@@ -9,7 +9,7 @@ int schemaPI(char *schemafile_map, table_t table*, int *cursor);
 int schemaFetchLine(char *schemafile_map, char *op, char *arg1, char *arg2, int *cursor);
 int renameColumn(char *arg1, char *arg2, table_t *table);
 int defineDV(char *arg1, char *arg2, table_t *table);
-int requirecolumn(char *arg1, char *arg2, table_t *table);
+int requireColumn(char *arg1, char *arg2, table_t *table);
 int defineFK(char *arg1, char *arg2, table_t *table);
 int defineVC(char *arg1, char *arg2, table_t *table);
 
